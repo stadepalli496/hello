5c8244309df3b8c3cd1c97f67090087023226774
Aditya Tadepalli <atadepalli.mb@gmail.com>
2023-08-24 11:52:18 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Aditya Tadepalli
+ * Date: 2023/08/24
+ *
+ * A simple name program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Aditya Tadepalli\n");
+
+  return 0;
+}
